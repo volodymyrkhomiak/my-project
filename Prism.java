7e266acfea112e4aaaa9227ac683e8d113f750e9
volodymyrkhomiak <33118690+volodymyrkhomiak@users.noreply.github.com>
2018-03-04 23:51:11 +0200

    Add files via upload

---
+++ Prism.java
@@ -0,0 +1,11 @@
+public class Prism extends foursquare {
+    private int z;
+
+    private int f1;
+    private int f;
+    private void Square(){
+        f1 = ((x*z)*2)+(y*z)*2);
+        f = f1+(sq*2);
+
+    }
+}
---
+++ foursquare.java
@@ -0,0 +1,10 @@
+public class foursquare {
+    private int x;
+    private int y;
+
+    private int sq;
+
+    private void Square(){
+    sq = x + y;
+    }
+}
