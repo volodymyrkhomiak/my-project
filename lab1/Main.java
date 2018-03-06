3f49b958403adbe08b0bbebbded9fb4821df51b2
volodymyrkhomiak <33118690+volodymyrkhomiak@users.noreply.github.com>
2018-03-06 10:45:40 +0200

    Update Main.java

--- lab1/Main.java
+++ lab1/Main.java
@@ -4,12 +4,28 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Ö³íà çà ê³ëîìåòð 10 ãðèâåíü");
+        System.out.println("vartist 10 grn za kilometr");
         Taxi s = new Taxi();
         s.show();
     }
 }
    
+
+import java.util.Scanner;
+
+public class Taxi {
+    public int information(){
+        return price;
+    }
+    private int shlyah;
+    private int price = 10;
+    void show(){
+        System.out.println("Vidstan: ");
+        Scanner sc = new Scanner(System.in);
+        shlyah = sc.nextInt();
+        System.out.println("Z vas " + shlyah*information()+" gryven");
+    }
+}
 
 
 
