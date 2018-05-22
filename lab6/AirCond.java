c0bf4236edf75a3af4f9ec92ec6d7e98b2ee980a
volodymyrkhomiak <33118690+volodymyrkhomiak@users.noreply.github.com>
2018-05-22 11:05:53 +0300

    Rename lab5/AirCond.java to lab6/AirCond.java

---
+++ lab6/AirCond.java
@@ -0,0 +1,15 @@
+
+
+public class AirCond   {
+    void run(){
+        System.out.println("Êîíäèö³îíåð çàïóñòèâñÿ");
+    }
+
+    void stop(){
+        System.out.println("Êîíäèö³îíåð çóïèíèâñÿ");
+    }
+
+    void set(){
+        System.out.println("Êîíäèö³îíåð íàëàøòîâàíèé");
+    }
+}
